811dbe46a36903a9cbc3762195ad2da28134614a
Joscha Henningsen <joscha.henningsen@tum.de>
2022-01-27 12:20:26 +0100

    buffered log of devices to database

---
+++ server/backend/device.go
@@ -0,0 +1,110 @@
+package backend
+
+import (
+	"context"
+	"github.com/TUM-Dev/Campus-Backend/model"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"sync"
+	"time"
+)
+
+// deviceBuffer stores all recent device calls in a buffer and flushes them to the database periodically
+type deviceBuffer struct {
+	lock     sync.Mutex
+	devices  map[string]*model.Devices // key is uuid
+	interval time.Duration             // flush interval
+}
+
+func (s *CampusServer) RunDeviceFlusher() error {
+	for {
+		time.Sleep(s.deviceBuf.interval)
+		err := s.deviceBuf.flush(s.db)
+		if err != nil {
+			log.WithError(err).Error("Error flushing device buffer")
+		}
+	}
+}
+
+func (b *deviceBuffer) add(device model.Devices) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	if _, exists := b.devices[device.UUID]; exists {
+		b.devices[device.UUID].Counter++
+	} else {
+		b.devices[device.UUID] = &device
+	}
+}
+
+// flush writes all buffered devices to the database
+func (b *deviceBuffer) flush(tx *gorm.DB) error {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	devices := make([]*model.Devices, 0, len(b.devices))
+	for _, device := range b.devices {
+		devices = append(devices, device)
+	}
+	if len(b.devices) == 0 {
+		return nil
+	}
+	// store devices in database, update if exists
+	err := tx.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "uuid"}}, // conflict on uuid -> update
+		DoUpdates: clause.AssignmentColumns([]string{"lastApi", "lastAccess", "osVersion", "appVersion"}),
+	}).Create(devices).Error
+	if err != nil {
+		log.WithError(err).Error("failed to flush device buffer")
+	}
+
+	// update number of calls for each device
+	err = tx.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "uuid"}},
+		DoUpdates: clause.Assignments(map[string]interface{}{"counter": gorm.Expr("counter+VALUES(counter)")}),
+	}).Create(devices).Error
+	if err != nil {
+		log.WithError(err).Error("failed to flush device buffer")
+	}
+	b.devices = make(map[string]*model.Devices)
+	return nil
+}
+
+// checkDevice checks if the device is approved (TODO: implement)
+func (s *CampusServer) checkDevice(ctx context.Context) error {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Error(codes.Internal, "can't extract metadata from request")
+	}
+	log.Println()
+	if len(md["x-device-id"]) == 0 && md["x-forwarded-for"][0] != "::1" {
+		return ErrNoDeviceID
+	}
+
+	method := md["x-campus-method"]
+	if len(method) == 0 {
+		return status.Error(codes.Internal, "can't extract method from request")
+	}
+
+	osVersion := "unknown"
+	if len(md["x-os-version"]) > 0 {
+		osVersion = md["x-os-version"][0]
+	}
+	appVersion := "unknown"
+	if len(md["x-app-version"]) > 0 {
+		appVersion = md["x-app-version"][0]
+	}
+
+	// log device to db
+	s.deviceBuf.add(model.Devices{
+		UUID:       md["x-device-id"][0],
+		LastAPI:    method[0],
+		LastAccess: time.Now(),
+		OsVersion:  osVersion,
+		AppVersion: appVersion,
+		Counter:    1, // initially 1 call, incremented on subsequent calls
+	})
+	return nil
+}
--- server/backend/rpcserver.go
+++ server/backend/rpcserver.go
@@ -7,12 +7,12 @@ import (
 	"github.com/TUM-Dev/Campus-Backend/model"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"gorm.io/gorm"
 	"net"
+	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -35,12 +35,18 @@ func (s *CampusServer) GRPCServe(l net.Listener) error {
 
 type CampusServer struct {
 	pb.UnimplementedCampusServer
-	db *gorm.DB
+	db        *gorm.DB
+	deviceBuf *deviceBuffer // deviceBuf stores all devices from recent request and flushes them to db
 }
 
 func New(db *gorm.DB) *CampusServer {
 	return &CampusServer{
 		db: db,
+		deviceBuf: &deviceBuffer{
+			lock:     sync.Mutex{},
+			devices:  make(map[string]*model.Devices),
+			interval: time.Second * 10,
+		},
 	}
 }
 
@@ -135,17 +141,4 @@ func (s *CampusServer) GetTopNews(ctx context.Context, _ *emptypb.Empty) (*pb.Ge
 		From:     nil,
 		To:       nil,
 	}, nil
-}
-
-// checkDevice checks if the device is approved (TODO: implement)
-func (s *CampusServer) checkDevice(ctx context.Context) error {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return status.Error(codes.Internal, "can't extract metadata from request")
-	}
-	if len(md["x-device-id"]) == 0 && md["x-forwarded-for"][0] != "::1" {
-		return ErrNoDeviceID
-	}
-	log.WithField("DeviceID", md["x-device-id"]).Info("Request from device")
-	return nil
 }
