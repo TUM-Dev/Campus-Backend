aaab9a47e35de29abcfa24e6417843ad245dd2cd
Tobias Jungmann <tobias.jungmann@tum.de>
2022-06-25 15:57:13 +0200

    added computeAverageForMealsInCafeteriasTags

--- server/backend/cron/averageRatingComputation.go
+++ server/backend/cron/averageRatingComputation.go
@@ -28,18 +28,61 @@ type averageCafeteriaTags struct {
 	Max         int8    `json:"max"`
 }
 
+type averageMealTags struct {
+	CafeteriaID int32   `gorm:"column:cafeteriaID;foreignKey:cafeteriaID;type:int;" json:"cafeteriaID"`
+	TagID       int32   `gorm:"foreignKey:tagRatingID;column:tagID;type:int" json:"tagID"`
+	MealID      int32   `gorm:"column:mealID;foreignKey:id;type:int;" json:"mealID"`
+	Average     float32 `json:"average"`
+	Min         int8    `json:"min"`
+	Max         int8    `json:"max"`
+}
+
 //regularly computes the average rating for every cafeteria
 func (c *CronService) averageRatingComputation() error {
-	computeAverageCafeteriaTags(c)
-	computeAverageForMealsInCafeterias(c)
 	computeAverageForCafeteria(c)
+	computeAverageForMealsInCafeterias(c)
 
-	//	computeAverageCafeteriaTags(c)
+	computeAverageCafeteriaTags(c)
+	computeAverageForMealsInCafeteriasTags(c)
+	computeAverageNameTags(c)
 	return nil
 }
 
-func computeAverageCafeteriaTags(c *CronService) {
+func computeAverageNameTags(c *CronService) {
 
+}
+
+func computeAverageForMealsInCafeteriasTags(c *CronService) {
+	var results []averageMealTags
+	err := c.db.Raw("SELECT mr.mealID as mealID, mr.cafeteriaID as cafeteriaID, mrt.tagID as tagID, AVG(mrt.rating) as average, MAX(mrt.rating) as max, MIN(mrt.rating) as min" +
+		" FROM meal_rating mr" +
+		" JOIN meal_rating_tags mrt ON mr.id = mrt.parentRating" +
+		" GROUP BY mr.cafeteriaID, mrt.tagID, mr.mealID").Scan(&results).Error
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		c.db.Where("1=1").Delete(&cafeteria_rating_models.MealRatingTagsAverage{})
+		for _, v := range results {
+			cafeteria := cafeteria_rating_models.MealRatingTagsAverage{
+				CafeteriaID: v.CafeteriaID,
+				MealID:      v.MealID,
+				Average:     v.Average,
+				TagID:       v.TagID,
+				Min:         v.Min,
+				Max:         v.Max,
+			} //todo add standard deviation
+
+			errCreate := c.db.Model(&cafeteria_rating_models.MealRatingsAverage{}).Create(&cafeteria).Error
+			if errCreate != nil {
+				log.Println(errCreate)
+			}
+
+		}
+	}
+}
+
+func computeAverageCafeteriaTags(c *CronService) {
 	var results []averageCafeteriaTags
 	err := c.db.Raw("SELECT cr.cafeteriaID as cafeteriaID, crt.tagID as tagID, AVG(crt.rating) as average, MAX(crt.rating) as max, MIN(crt.rating) as min" +
 		" FROM cafeteria_rating cr" +
--- server/model/cafeteria_rating_models/mealRatingTagsAverage.go
+++ server/model/cafeteria_rating_models/mealRatingTagsAverage.go
@@ -17,8 +17,8 @@ type MealRatingTagsAverage struct {
 	TagID       int32   `gorm:"column:tagID;foreignKey:tagID;type:int;" json:"tagID"`
 	MealID      int32   `gorm:"column:mealID;foreignKey:mealID;type:int;" json:"mealID"`
 	Average     float32 `gorm:"column:average;type:float;" json:"average" `
-	Min         int     `gorm:"column:min;type:int;" json:"min"`
-	Max         int     `gorm:"column:max;type:int;" json:"max"`
+	Min         int8    `gorm:"column:min;type:int;" json:"min"`
+	Max         int8    `gorm:"column:max;type:int;" json:"max"`
 }
 
 // TableName sets the insert table name for this struct type
