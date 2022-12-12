fe2dbd8127a1aff095cf5ad45e725cade2c3eb16
Alexander Stephan <alexander.stephan@tum.de>
2022-12-12 22:56:02 +0100

    Update path

--- client/publicServer/client.go
+++ client/publicServer/client.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"crypto/x509"
-	pb "github.com/TUM-Dev/Campus-Backend/api"
+	pb "github.com/TUM-Dev/Campus-Backend/server/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
