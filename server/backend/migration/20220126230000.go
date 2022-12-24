31c459efe71f95d2530287d9e33b4152fa7393a3
Fabian Sauter <sauter.fabian@mailbox.org>
2022-12-24 08:58:56 +0100

    Added missing roomfinder auto migration

--- server/backend/migration/20220126230000.go
+++ server/backend/migration/20220126230000.go
@@ -1,6 +1,7 @@
 package migration
 
 import (
+	"github.com/TUM-Dev/Campus-Backend/model"
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
 )
@@ -11,6 +12,12 @@ func (m TumDBMigrator) migrate20220126230000() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "20220126230000",
 		Migrate: func(tx *gorm.DB) error {
+			if err := tx.AutoMigrate(
+				&model.RoomfinderRooms{},
+			); err != nil {
+				return err
+			}
+
 			return tx.Exec("CREATE FULLTEXT INDEX `search_index` ON `roomfinder_rooms` (`info`, `address`, `room_code`)").Error
 		},
 		Rollback: func(tx *gorm.DB) error {
