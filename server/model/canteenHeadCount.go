f8c5c09c106b1b338133db804400dc0ecfb5f77f
Fabian Sauter <sauter.fabian@mailbox.org>
2022-12-24 08:40:08 +0100

    Fixed canteen_id primary key for mysql

--- server/model/canteenHeadCount.go
+++ server/model/canteenHeadCount.go
@@ -6,7 +6,7 @@ import (
 
 // CanteenHeadCount stores all available people counts for available canteens. The CanteenId represents the same ID, as for the canteen inside the eat-api.
 type CanteenHeadCount struct {
-	CanteenId string    `gorm:"primary_key;column:canteen_id;type:text;not null;" json:"canteen_id"`
+	CanteenId string    `gorm:"primary_key;column:canteen_id;type:varchar(64);not null;" json:"canteen_id"`
 	Count     uint32    `gorm:"column:count;type:int;not null;" json:"count"`
 	MaxCount  uint32    `gorm:"column:max_count;type:int;not null;" json:"max_count"`
 	Percent   float32   `gorm:"column:percent;type:float;not null;" json:"percent"`
