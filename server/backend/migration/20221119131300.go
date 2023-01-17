da1649555205b82d9e73ce5d3ccab152b1ea6a4b
Anton Wyrowski <anton.wyrowski@gmail.com>
2023-01-17 10:49:55 +0100

    added safe enum migration

--- server/backend/migration/20221119131300.go
+++ server/backend/migration/20221119131300.go
@@ -4,15 +4,12 @@ import (
 	"database/sql"
 	_ "embed"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/TUM-Dev/Campus-Backend/server/backend/cron"
 	"github.com/TUM-Dev/Campus-Backend/server/model"
 	"github.com/go-gormigrate/gormigrate/v2"
 	"github.com/guregu/null"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
-	"strings"
 )
 
 //migrate20221115000000
@@ -38,22 +35,10 @@ func (m TumDBMigrator) migrate20221119131300() *gormigrate.Migration {
 				return err
 			}
 
-			if cTypes, err := tx.Migrator().ColumnTypes(&model.Crontab{}); err == nil {
-				types, _ := getCrontabEnumTypes(cTypes)
+			err := SafeEnumMigrate(tx, &model.Crontab{}, "type", "iosNotifications", "iosActivityReset")
 
-				if !contains(types, "iosNotifications") {
-					if err := tx.Exec(fmt.Sprintf("alter table campus_db.crontab modify type enum (%s) null;", typesToString(types, "iosNotifications"))).Error; err != nil {
-						log.Info(err.Error())
-						return err
-					}
-				}
-
-				if !contains(types, "iosActivityReset") {
-					if err := tx.Exec(fmt.Sprintf("alter table campus_db.crontab modify type enum (%s) null;", typesToString(types, "iosActivityReset"))).Error; err != nil {
-						log.Info(err.Error())
-						return err
-					}
-				}
+			if err != nil {
+				return err
 			}
 
 			var priorities []model.IOSSchedulingPriority
@@ -70,7 +55,7 @@ func (m TumDBMigrator) migrate20221119131300() *gormigrate.Migration {
 				return err
 			}
 
-			err := tx.Create(&model.Crontab{
+			err = tx.Create(&model.Crontab{
 				Interval: 60,
 				Type:     null.String{NullString: sql.NullString{String: cron.IOS_NOTIFICATIONS, Valid: true}},
 			}).Error
@@ -114,80 +99,18 @@ func (m TumDBMigrator) migrate20221119131300() *gormigrate.Migration {
 				return err
 			}
 
-			if cTypes, err := tx.Migrator().ColumnTypes(&model.Crontab{}); err == nil {
-				types, _ := getCrontabEnumTypes(cTypes)
-
-				if !contains(types, "iosNotifications") {
-					if err := tx.Exec(fmt.Sprintf("alter table campus_db.crontab modify type enum (%s) null;", rollback(types, "iosNotifications"))).Error; err != nil {
-						log.Info(err.Error())
-						return err
-					}
-				}
-
-				if !contains(types, "iosActivityReset") {
-					if err := tx.Exec(fmt.Sprintf("alter table campus_db.crontab modify type enum (%s) null;", rollback(types, "iosActivityReset"))).Error; err != nil {
-						log.Info(err.Error())
-						return err
-					}
-				}
-			}
-
 			err := tx.Delete(&model.Crontab{}, "type = ? AND interval = ?", cron.IOS_NOTIFICATIONS, 60).Error
 			if err != nil {
-				log.Error(err.Error())
+				return err
 			}
 
-			return tx.Delete(&model.Crontab{}, "type = ? AND interval = ?", cron.IOS_ACTIVITY_RESET, 86400).Error
+			err = tx.Delete(&model.Crontab{}, "type = ? AND interval = ?", cron.IOS_ACTIVITY_RESET, 86400).Error
+
+			if err != nil {
+				return err
+			}
+
+			return SafeEnumRollback(tx, &model.Crontab{}, "type", "iosNotifications", "iosActivityReset")
 		},
 	}
-}
-
-func typesToString(types []string, plus string) string {
-	var str string
-	for _, t := range types {
-		str += fmt.Sprintf("%s,", t)
-	}
-
-	str += fmt.Sprintf("'%s'", plus)
-
-	return str
-}
-
-func rollback(types []string, search string) string {
-	var str string
-
-	for _, eType := range types {
-		if eType != fmt.Sprintf("'%s'", search) {
-			str += fmt.Sprintf("%s,", eType)
-		}
-	}
-
-	return strings.TrimRight(str, ",")
-}
-
-func contains(types []string, search string) bool {
-	for _, t := range types {
-		if t == fmt.Sprintf("'%s'", search) {
-			return true
-		}
-	}
-
-	return false
-}
-
-func getCrontabEnumTypes(types []gorm.ColumnType) ([]string, error) {
-	for _, t := range types {
-		if t.Name() == "type" {
-			if cType, ok := t.ColumnType(); ok {
-				leftTrimmed := strings.TrimLeft(cType, "enum")
-				trimmed := strings.Trim(leftTrimmed, "()")
-
-				return strings.Split(trimmed, ","), nil
-			} else {
-				return []string{}, errors.New("could not get column type")
-			}
-		}
-	}
-
-	return []string{}, errors.New("could not find column type")
 }
