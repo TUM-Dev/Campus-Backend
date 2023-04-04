dacc8ae07416fae55c81a1f11bc3ea3e47886db4
Anton Wyrowski <anton.wyrowski@gmail.com>
2023-04-04 11:21:21 +0200

    improved average grade

--- server/backend/approximated_average_grade/avgGradeRepository.go
+++ server/backend/approximated_average_grade/avgGradeRepository.go
@@ -1,14 +1,24 @@
 package approximated_average_grade
 
 import (
+	"fmt"
 	pb "github.com/TUM-Dev/Campus-Backend/server/api"
 	"github.com/TUM-Dev/Campus-Backend/server/model"
 	"gorm.io/gorm"
+	"strconv"
+	"strings"
 )
 
 type Repository struct {
 	DB *gorm.DB
 }
+
+var customContainsCourses = map[string]int32{
+	"Praktikum": 10,
+	"Seminar":   5,
+}
+
+const FallbackEcts = 6
 
 func (repo *Repository) GetCalculationSteps(grades []model.Grade) []*pb.ApproximatedAverageGradeCalculationStep {
 	var calculationSteps []*pb.ApproximatedAverageGradeCalculationStep
@@ -16,27 +26,132 @@ func (repo *Repository) GetCalculationSteps(grades []model.Grade) []*pb.Approxim
 	for _, grade := range grades {
 		var lecture model.CrawlerLecture
 
-		tx := repo.DB.First(&lecture, "name like ?", "%"+grade.LectureTitle+"%")
-		if tx.Error != nil {
-			errorMessage := "Could not find a lecture for this grade. Won't be included in the calculation."
-			calculationSteps = append(calculationSteps, &pb.ApproximatedAverageGradeCalculationStep{
-				LectureTitle: grade.LectureTitle,
-				Grade:        1.0,
-				Weight:       0,
-				Ects:         0,
-				ErrorMessage: &errorMessage,
-			})
-		} else {
-			calculationSteps = append(calculationSteps, &pb.ApproximatedAverageGradeCalculationStep{
-				LectureTitle: lecture.Name,
-				Grade:        1.0,
-				Weight:       1,
-				Ects:         5,
-			})
+		errorMessage := ""
+		detailMessage := ""
+
+		tx := repo.DB.
+			Where("lecture_id = ?", grade.LectureNumber).
+			Or("name like ?", "%"+grade.LectureTitle+"%").
+			Or("name like ?", "%"+grade.LectureTitle+"%").
+			First(&lecture)
+
+		lectureFound := tx.Error == nil
+		customContainsEcts, isCustomCourse := isCustomContainsCourse(grade.LectureTitle)
+
+		if !lectureFound && !isCustomCourse {
+			errorMessage = fmt.Sprintf("Could not find lecture %s. Using default ECTS value of %d", grade.LectureTitle, FallbackEcts)
 		}
+
+		floatGrade, err := parseGrade(grade.Grade)
+		if err != nil {
+			errorMessage = "Could not parse grade " + grade.Grade + " to float"
+		}
+
+		if floatGrade > 4.0 {
+			// grade is not counted because exam not passed
+			continue
+		}
+
+		floatWeight := 1.0
+
+		if lectureFound {
+			floatWeight, err = parseCommaStringToFloat(lecture.Weight)
+			if err != nil {
+				errorMessage = "Could not parse lecture weight " + lecture.Weight + " to float"
+			}
+		}
+
+		ectsValue := int32(FallbackEcts)
+
+		if lectureFound {
+			var detailMsg *string
+
+			ectsValue, detailMsg, err = parseEctsStringToInt(lecture.ECTS)
+			if err != nil {
+				errorMessage = "Could not parse lecture ECTS " + lecture.ECTS + " to int"
+			}
+			if detailMsg != nil {
+				detailMessage = *detailMsg
+			}
+		}
+
+		if !lectureFound && isCustomCourse {
+			ectsValue = customContainsEcts
+		}
+
+		calculationStep := &pb.ApproximatedAverageGradeCalculationStep{
+			LectureTitle: grade.LectureTitle,
+			Grade:        floatGrade,
+			Weight:       floatWeight,
+			Ects:         ectsValue,
+		}
+
+		if len(errorMessage) > 0 {
+			calculationStep.ErrorMessage = &errorMessage
+		}
+
+		if len(detailMessage) > 0 {
+			calculationStep.InfoMessage = &detailMessage
+		}
+
+		calculationSteps = append(calculationSteps, calculationStep)
 	}
 
 	return calculationSteps
+}
+
+func isCustomContainsCourse(lectureTitle string) (int32, bool) {
+	for key, value := range customContainsCourses {
+		if strings.Contains(strings.ToLower(lectureTitle), strings.ToLower(key)) {
+			return value, true
+		}
+	}
+
+	return 0, false
+}
+
+func parseEctsStringToInt(stringEcts string) (int32, *string, error) {
+	ectsParts := strings.Split(stringEcts, ",")
+
+	if len(ectsParts) > 1 {
+		detailMessage := fmt.Sprintf("Found %d ECTS values for one lecture. Selecting the first one of %s.", len(ectsParts), strings.Join(ectsParts, ", "))
+		ectsValue, err := strconv.Atoi(ectsParts[0])
+
+		if err != nil {
+			return 0, nil, err
+		}
+
+		return int32(ectsValue), &detailMessage, nil
+	}
+
+	ectsValue, err := strconv.Atoi(stringEcts)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return int32(ectsValue), nil, nil
+}
+
+func parseGrade(gradeString string) (float64, error) {
+	switch gradeString {
+	case "N":
+		return 5.0, nil
+	case "B":
+		return 5.0, nil
+	}
+
+	return parseCommaStringToFloat(gradeString)
+}
+
+func parseCommaStringToFloat(stringFloat string) (float64, error) {
+	commaReplaced := strings.Replace(stringFloat, ",", ".", -1)
+
+	floatValue, err := strconv.ParseFloat(commaReplaced, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return floatValue, nil
 }
 
 func NewRepository(db *gorm.DB) *Repository {
--- server/backend/approximated_average_grade/avgGradeService.go
+++ server/backend/approximated_average_grade/avgGradeService.go
@@ -3,6 +3,8 @@ package approximated_average_grade
 import (
 	pb "github.com/TUM-Dev/Campus-Backend/server/api"
 	"github.com/TUM-Dev/Campus-Backend/server/backend/campus_api"
+	"github.com/TUM-Dev/Campus-Backend/server/model"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
@@ -24,19 +26,54 @@ func (service *Service) HandleGetApproximatedAverageGrade(req *pb.GetApproximate
 		return nil, ErrCouldNotFetchGrades
 	}
 
-	steps := service.Repository.GetCalculationSteps(grades.Grades)
+	idMap := gradesToIdMap(grades)
 
-	sum := 0.0
-	for _, step := range steps {
-		sum += step.Grade * float64(step.Ects) * step.Weight
+	var studyIdGrades []*pb.ApproximatedAverageGradePerStudy
+
+	for id, grades := range *idMap {
+		if len(grades) == 0 {
+			continue
+		}
+
+		steps := service.Repository.GetCalculationSteps(grades)
+
+		gradeEctsSum := 0.0
+		ectsSum := int32(0)
+
+		for _, step := range steps {
+			gradeEctsSum += step.Grade * float64(step.Ects) * step.Weight
+			ectsSum += step.Ects
+		}
+
+		average := 0.0
+
+		if ectsSum > 0 {
+			average = gradeEctsSum / float64(ectsSum)
+		}
+
+		averageGradePerStudy := pb.ApproximatedAverageGradePerStudy{
+			StudyId:          id,
+			AverageGrade:     average,
+			CalculationSteps: steps,
+		}
+
+		studyIdGrades = append(studyIdGrades, &averageGradePerStudy)
 	}
 
-	average := sum / float64(len(steps))
-
 	return &pb.GetApproximatedAverageGradeReply{
-		AverageGrade:     average,
-		CalculationSteps: steps,
+		Studies: studyIdGrades,
 	}, nil
+}
+
+func gradesToIdMap(grades *model.Grades) *map[string][]model.Grade {
+	gradesMap := map[string][]model.Grade{}
+
+	for _, grade := range grades.Grades {
+		gradesMap[grade.StudyID] = append(gradesMap[grade.StudyID], grade)
+		log.Infof(grade.StudyID)
+	}
+
+	return &gradesMap
 }
 
 func NewService(db *gorm.DB) *Service {
