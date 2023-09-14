142caadf08bd1e532a40528969295887799fd58d
Frank Elsinga <frank@elsinga.de>
2023-09-14 03:29:59 +0200

    ran the installBuf.bash script in ci

--- server/api/generate.bash
+++ server/api/generate.bash
@@ -10,7 +10,7 @@ buf mod update
 buf generate
 
 echo making sure the openapi document points to the valid api
-sed -i '1 a "basePath": "/v1",' ./tumdev/campus_backend.swagger.json
+grep -q '"basePath": "/v1"' ./tumdev/campus_backend.swagger.json || sed -i '1 a "basePath": "/v1",' ./tumdev/campus_backend.swagger.json
 
 echo making sure that all artifacts we don\'t need are cleaned up
 rm -f google/api/*.go
