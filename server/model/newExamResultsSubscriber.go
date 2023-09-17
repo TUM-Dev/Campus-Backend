346d799fb68e6d4cf6e7c1c680c9ddce6af1cff7
Anton Wyrowski <anton.wyrowski@gmail.com>
2023-09-17 18:45:45 +0200

    fix: directly added primary key to callback url

--- server/model/newExamResultsSubscriber.go
+++ server/model/newExamResultsSubscriber.go
@@ -6,8 +6,7 @@ import (
 )
 
 type NewExamResultsSubscriber struct {
-	ID             int32          `gorm:"primary_key;AUTO_INCREMENT;" json:"id"`
-	CallbackUrl    string         `json:"callbackUrl"`
+	CallbackUrl    string         `gorm:"primary_key" json:"callbackUrl"`
 	ApiKey         sql.NullString `json:"-"`
 	CreatedAt      time.Time      `gorm:"autoCreateTime" json:"createdAt"`
 	LastNotifiedAt sql.NullTime   `json:"lastNotifiedAt"`
