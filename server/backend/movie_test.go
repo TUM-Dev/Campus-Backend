4c91ba212b6d7b648667b7113df9ae491ee3c4b5
Frank Elsinga <frank@elsinga.de>
2023-09-19 18:35:20 +0200

    fixed issues in the rebase

--- server/backend/movie_test.go
+++ server/backend/movie_test.go
@@ -46,7 +46,7 @@ var (
 		Date:        timestamppb.New(time.Now()),
 		Created:     timestamppb.New(time.Now()),
 		Title:       "Mission Impossible 4 - Ghost Protocol",
-		Year:        "2011",
+		ReleaseYear: "2011",
 		Runtime:     "133 min",
 		Genre:       "Action, Adventure, Thriller",
 		Director:    "Brad Bird",
@@ -63,7 +63,7 @@ var (
 		Date:        timestamppb.New(time.Now()),
 		Created:     timestamppb.New(time.Now()),
 		Title:       "Mission Impossible 5 - Rogue Nation",
-		Year:        "2015",
+		ReleaseYear: "2015",
 		Runtime:     "131 min",
 		Genre:       "Action, Adventure, Thriller",
 		Director:    "Christopher McQuarrie",
@@ -82,8 +82,8 @@ func (s *MovieSuite) Test_GetMoviesAll() {
 	s.mock.ExpectQuery("SELECT `kino`.`kino`,`kino`.`date`,`kino`.`created`,`kino`.`title`,`kino`.`year`,`kino`.`runtime`,`kino`.`genre`,`kino`.`director`,`kino`.`actors`,`kino`.`rating`,`kino`.`description`,`kino`.`trailer`,`kino`.`cover`,`kino`.`link`,`Files`.`file` AS `Files__file`,`Files`.`name` AS `Files__name`,`Files`.`path` AS `Files__path`,`Files`.`downloads` AS `Files__downloads`,`Files`.`url` AS `Files__url`,`Files`.`downloaded` AS `Files__downloaded` FROM `kino` LEFT JOIN `files` `Files` ON `kino`.`cover` = `Files`.`file` WHERE kino > ?").
 		WithArgs(-1).
 		WillReturnRows(sqlmock.NewRows([]string{"kino", "date", "created", "title", "year", "runtime", "genre", "director", "actors", "rating", "description", "trailer", "cover", "link", "Files__file", "Files__name", "Files__path", "Files__downloads", "Files__url", "Files__downloaded"}).
-			AddRow(movie2.MovieId, movie2.Date.AsTime(), movie2.Created.AsTime(), movie2.Title, movie2.Year, movie2.Runtime, movie2.Genre, movie2.Director, movie2.Actors, movie2.ImdbRating, movie2.Description, nil, movie2.CoverId, movie2.Link, movie2.CoverId, movie2.CoverName, movie2.CoverPath, 1, "", 1).
-			AddRow(movie1.MovieId, movie1.Date.AsTime(), movie1.Created.AsTime(), movie1.Title, movie1.Year, movie1.Runtime, movie1.Genre, movie1.Director, movie1.Actors, movie1.ImdbRating, movie1.Description, nil, movie1.CoverId, movie1.Link, movie1.CoverId, movie1.CoverName, movie1.CoverPath, 1, "", 1))
+			AddRow(movie2.MovieId, movie2.Date.AsTime(), movie2.Created.AsTime(), movie2.Title, movie2.ReleaseYear, movie2.Runtime, movie2.Genre, movie2.Director, movie2.Actors, movie2.ImdbRating, movie2.Description, nil, movie2.CoverId, movie2.Link, movie2.CoverId, movie2.CoverName, movie2.CoverPath, 1, "", 1).
+			AddRow(movie1.MovieId, movie1.Date.AsTime(), movie1.Created.AsTime(), movie1.Title, movie1.ReleaseYear, movie1.Runtime, movie1.Genre, movie1.Director, movie1.Actors, movie1.ImdbRating, movie1.Description, nil, movie1.CoverId, movie1.Link, movie1.CoverId, movie1.CoverName, movie1.CoverPath, 1, "", 1))
 	response, err := server.GetMovies(context.Background(), &pb.GetMoviesRequest{LastId: -1})
 	require.NoError(s.T(), err)
 	require.Equal(s.T(), &pb.GetMoviesReply{Movies: []*pb.Movie{&movie2, &movie1}}, response)
@@ -94,7 +94,7 @@ func (s *MovieSuite) Test_GetMoviesOne() {
 	s.mock.ExpectQuery("SELECT `kino`.`kino`,`kino`.`date`,`kino`.`created`,`kino`.`title`,`kino`.`year`,`kino`.`runtime`,`kino`.`genre`,`kino`.`director`,`kino`.`actors`,`kino`.`rating`,`kino`.`description`,`kino`.`trailer`,`kino`.`cover`,`kino`.`link`,`Files`.`file` AS `Files__file`,`Files`.`name` AS `Files__name`,`Files`.`path` AS `Files__path`,`Files`.`downloads` AS `Files__downloads`,`Files`.`url` AS `Files__url`,`Files`.`downloaded` AS `Files__downloaded` FROM `kino` LEFT JOIN `files` `Files` ON `kino`.`cover` = `Files`.`file` WHERE kino > ?").
 		WithArgs(1).
 		WillReturnRows(sqlmock.NewRows([]string{"kino", "date", "created", "title", "year", "runtime", "genre", "director", "actors", "rating", "description", "trailer", "cover", "link", "Files__file", "Files__name", "Files__path", "Files__downloads", "Files__url", "Files__downloaded"}).
-			AddRow(movie1.MovieId, movie1.Date.AsTime(), movie1.Created.AsTime(), movie1.Title, movie1.Year, movie1.Runtime, movie1.Genre, movie1.Director, movie1.Actors, movie1.ImdbRating, movie1.Description, nil, movie1.CoverId, movie1.Link, movie1.CoverId, movie1.CoverName, movie1.CoverPath, 1, "", 1))
+			AddRow(movie1.MovieId, movie1.Date.AsTime(), movie1.Created.AsTime(), movie1.Title, movie1.ReleaseYear, movie1.Runtime, movie1.Genre, movie1.Director, movie1.Actors, movie1.ImdbRating, movie1.Description, nil, movie1.CoverId, movie1.Link, movie1.CoverId, movie1.CoverName, movie1.CoverPath, 1, "", 1))
 	response, err := server.GetMovies(context.Background(), &pb.GetMoviesRequest{LastId: 1})
 	require.NoError(s.T(), err)
 	require.Equal(s.T(), &pb.GetMoviesReply{Movies: []*pb.Movie{&movie1}}, response)
--- server/model/kino.go
+++ server/model/kino.go
@@ -8,7 +8,7 @@ import (
 
 // Kino stores all movies
 type Kino struct {
-	Id          int32       `gorm:"primary_key;AUTO_INCREMENT;column:kino;type:int;not null;"`
+	Id          int64       `gorm:"primary_key;AUTO_INCREMENT;column:kino;type:int;not null;"`
 	Date        time.Time   `gorm:"column:date;type:datetime;not null;"`
 	Created     time.Time   `gorm:"column:created;type:timestamp;not null;default:CURRENT_TIMESTAMP"`
 	Title       string      `gorm:"column:title;type:text;not null;"`
@@ -20,7 +20,7 @@ type Kino struct {
 	ImdbRating  string      `gorm:"column:rating;type:varchar(4);not null;"`
 	Description string      `gorm:"column:description;type:text;not null;"`
 	Trailer     null.String `gorm:"column:trailer"`
-	FilesID     int32       `gorm:"column:cover"`
+	FilesID     int64       `gorm:"column:cover"`
 	Files       Files       `gorm:"foreignKey:FilesID;references:file"`
 	Link        string      `gorm:"column:link;type:varchar(190);not null;unique;"`
 }
