e342277f65a93377f52a5de84dd0a090c2650f27
Frank Elsinga <frank@elsinga.de>
2023-09-19 16:09:45 +0200

    fixed naming things imdb if they should be omdb

--- server/backend/cron/movies.go
+++ server/backend/cron/movies.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -63,15 +64,15 @@ func (c *CronService) movieCron() error {
 				continue
 			}
 
-			// populate extra data from imdb
-			imdbID, err := extractTUFilmWebsite(item.Link)
+			// populate extra data from omdb
+			imdbID, err := extractImdbIDFromTUFilmWebsite(item.Link)
 			if err != nil {
 				log.WithFields(logFields).WithError(err).Error("error while finding imdb id")
 				continue
 			}
-			imdbMovie, err := getIMDB(imdbID)
+			omdbMovie, err := getOmdbMovie(imdbID)
 			if err != nil {
-				log.WithFields(logFields).WithError(err).Error("error while getting imdb movie")
+				log.WithFields(logFields).WithError(err).Error("error while getting omdb movie")
 				continue
 			}
 
@@ -90,13 +91,13 @@ func (c *CronService) movieCron() error {
 			movie := model.Kino{
 				Date:        date,
 				Title:       item.Title,
-				Year:        imdbMovie.Year,
-				Runtime:     imdbMovie.Runtime,
-				Genre:       imdbMovie.Genre,
-				Director:    imdbMovie.Director,
-				Actors:      imdbMovie.Actors,
-				ImdbRating:  imdbMovie.ImdbRating,
-				Description: imdbMovie.Plot, // we get this from imdb as tu-fim does truncate their plot
+				Year:        omdbMovie.ReleaseYear,
+				Runtime:     omdbMovie.Runtime,
+				Genre:       omdbMovie.Genre,
+				Director:    omdbMovie.Director,
+				Actors:      omdbMovie.Actors,
+				ImdbRating:  omdbMovie.ImdbRating,
+				Description: omdbMovie.Plot, // we get this from imdb as tu-fim does truncate their plot
 				FilesID:     file.File,
 				Files:       file,
 				Link:        item.Link,
@@ -112,18 +113,18 @@ func (c *CronService) movieCron() error {
 	return nil
 }
 
-type imdbResults struct {
-	Year       string
-	Runtime    string
-	Genre      string
-	Director   string
-	Actors     string
-	Plot       string
-	ImdbRating string `json:"imdbRating"`
+type omdbResults struct {
+	ReleaseYear string `json:"Year"`
+	Runtime     string
+	Genre       string
+	Director    string
+	Actors      string
+	Plot        string
+	ImdbRating  string `json:"imdbRating"`
 }
 
-func getIMDB(id string) (*imdbResults, error) {
-	url := "https://www.omdbapi.com/?r=json&v=1&i=" + id + "&apikey=" + os.Getenv("OMDB_API_KEY")
+func getOmdbMovie(id string) (*omdbResults, error) {
+	url := fmt.Sprintf("https://www.omdbapi.com/?r=json&v=1&i=%s&apikey=%s", id, os.Getenv("OMDB_API_KEY"))
 	resp, err := http.Get(url)
 	if err != nil {
 		log.WithField("url", url).WithError(err).Error("Error while getting response for request")
@@ -140,8 +141,8 @@ func getIMDB(id string) (*imdbResults, error) {
 			log.WithError(err).Warn("Unable to read http body")
 			return nil, err
 		} else {
-			log.WithField("status", resp.StatusCode).WithField("status", resp.Status).WithField("body", string(body)).Error("error while getting imdb movie")
-			return nil, errors.New("error while getting imdb movie")
+			log.WithField("status", resp.StatusCode).WithField("status", resp.Status).WithField("body", string(body)).Error("error while getting omdb movie")
+			return nil, errors.New("error while getting omdb movie")
 		}
 	}
 	defer func(Body io.ReadCloser) {
@@ -151,18 +152,18 @@ func getIMDB(id string) (*imdbResults, error) {
 		}
 	}(resp.Body)
 	// parse the response body
-	var res imdbResults
+	var res omdbResults
 	err = json.NewDecoder(resp.Body).Decode(&res)
 	if err != nil {
-		log.WithField("url", url).WithError(err).Error("Error while unmarshalling imdbResults")
+		log.WithField("url", url).WithError(err).Error("Error while unmarshalling omdbResults")
 		return nil, err
 	}
 	return &res, nil
 }
 
-// extractTUFilmWebsite scrapes the imdb id and fullDescription from the tu-film website
+// extractImdbIDFromTUFilmWebsite scrapes the imdb id and fullDescription from the tu-film website
 // url: url of the tu-film website, e.g. https://www.tu-film.de/programm/view/1204
-func extractTUFilmWebsite(url string) (string, error) {
+func extractImdbIDFromTUFilmWebsite(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", errors.New("error while getting response for request")
@@ -191,7 +192,7 @@ func extractTUFilmWebsite(url string) (string, error) {
 	if imdbLinks.Length() > 1 {
 		log.WithField("url", url).Warn("more than one imdb link found. using first one")
 	}
-	// extrat the imdb id from the link
+	// extract the imdb id from the link
 	href, _ := imdbLinks.First().Attr("href")
 	href = strings.TrimSuffix(href, "/")
 	hrefParts := strings.Split(href, "/")
