5c88e96b2c2b2e466e9e5dff8af4c001677704ec
Frank Elsinga <frank@elsinga.de>
2023-09-21 22:58:06 +0200

    made the logging path of fileDownload.go more straight-forward

--- server/backend/cron/fileDownload.go
+++ server/backend/cron/fileDownload.go
@@ -2,11 +2,12 @@ package cron
 
 import (
 	"bytes"
-	"fmt"
+	"errors"
 	"image"
 	"io"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/TUM-Dev/Campus-Backend/server/model"
@@ -19,13 +20,24 @@ import (
 // fileDownloadCron Downloads all files that are not marked as finished in the database.
 func (c *CronService) fileDownloadCron() error {
 	var files []model.Files
-	err := c.db.Find(&files, "downloaded = 0").Scan(&files).Error
-	if err != nil && err != gorm.ErrRecordNotFound {
+	err := c.db.Find(&files, "downloaded = 0").Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		log.WithError(err).Error("Could not get files from database")
 		return err
 	}
-	for i := range files {
-		if files[i].URL.Valid {
-			c.downloadFile(files[i])
+	for _, file := range files {
+		if !file.URL.Valid {
+			log.WithField("fileId", file.File).Info("skipping file without url")
+			continue
+		}
+		log.WithField("url", file.URL.String).Info("downloading file")
+		if err := downloadFile(&file); err != nil {
+			log.WithError(err).WithField("url", file.URL.String).Warn("Could not download file")
+			continue
+		}
+		if err = c.db.Model(&model.Files{URL: file.URL}).Update("downloaded", true).Error; err != nil {
+			log.WithError(err).Error("Could not set image to downloaded.")
+			continue
 		}
 	}
 	return nil
@@ -34,51 +46,40 @@ func (c *CronService) fileDownloadCron() error {
 // downloadFile Downloads a file, marks it downloaded and resizes it if it's an image.
 // url: download url of the file
 // name: target name of the file
-func (c *CronService) downloadFile(file model.Files) {
-	if !file.URL.Valid {
-		log.WithField("fileId", file.File).Info("skipping file without url")
-	}
-	url := file.URL.String
-	log.WithField("url", url).Info("downloading file")
-	resp, err := http.Get(url)
+func downloadFile(file *model.Files) error {
+	resp, err := http.Get(file.URL.String)
 	if err != nil {
-		log.WithError(err).WithField("url", url).Warn("Could not download image")
-		return
+		log.WithError(err).WithField("url", file.URL.String).Warn("Could not download image")
+		return err
 	}
 	// read body here because we can't exhaust the io.reader twice
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.WithError(err).Warn("Unable to read http body")
-		return
+		return err
 	}
 
-	// resize if file is image
-	mime := mimetype.Detect(body)
-	if strings.HasPrefix(mime.String(), "image/") {
+	// in our case resolves to /Storage/news/newspread/1234abc.jpg
+	dstPath := path.Join(StorageDir, file.Path, file.Name)
+	if strings.HasPrefix(mimetype.Detect(body).String(), "image/") {
+		// save with resizing
 		downloadedImg, _, err := image.Decode(bytes.NewReader(body))
 		if err != nil {
-			log.WithError(err).WithField("url", url).Warn("Couldn't decode source image")
-			return
+			log.WithError(err).WithField("url", file.URL.String).Warn("Couldn't decode source image")
+			return err
 		}
 
-		// in our case resolves to /Storage/news/newspread/1234abc.jpg
-		dstFileName := fmt.Sprintf("%s%s", file.Path, file.Name)
-		dstImage := imaging.Resize(downloadedImg, 1280, 0, imaging.Lanczos)
-		err = imaging.Save(dstImage, StorageDir+dstFileName, imaging.JPEGQuality(75))
-		if err != nil {
-			log.WithError(err).WithField("url", url).Warn("Could not save image file")
-			return
+		resizedImage := imaging.Resize(downloadedImg, 1280, 0, imaging.Lanczos)
+		if err = imaging.Save(resizedImage, dstPath, imaging.JPEGQuality(75)); err != nil {
+			log.WithError(err).WithField("url", file.URL.String).Warn("Could not save image file")
+			return err
 		}
 	} else {
 		// save without resizing image
-		err = os.WriteFile(fmt.Sprintf("%s%s", file.Path, file.Name), body, 0644)
-		if err != nil {
+		if err := os.WriteFile(dstPath, body, 0644); err != nil {
 			log.WithError(err).Error("Can't save file to disk")
-			return
+			return err
 		}
 	}
-	err = c.db.Model(&model.Files{}).Where("url = ?", url).Update("downloaded", true).Error
-	if err != nil {
-		log.WithError(err).Error("Could not set image to downloaded.")
-	}
+	return nil
 }
