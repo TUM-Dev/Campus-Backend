411564e7713cc8e16917478417eef60ae35326a6
Tobias Jungmann <tobias.jungmann@tum.de>
2023-10-05 10:59:15 +0200

    added state of models in migration

--- server/backend/migration/20231003000000.go
+++ server/backend/migration/20231003000000.go
@@ -32,6 +32,37 @@ type nameTag struct {
 	CanBeIncluded  []string `json:"canbeincluded"`
 }
 
+// State of the models at this migration
+type CafeteriaRatingTagOption struct {
+	CafeteriaRatingsTagOption int64  `gorm:"primary_key;AUTO_INCREMENT;column:cafeteriaRatingTagOption;type:int;not null;" json:"canteenRatingTagOption"`
+	DE                        string `gorm:"column:DE;text;default:de;not null;" json:"DE"`
+	EN                        string `gorm:"column:EN;text;default:en;not null;" json:"EN"`
+}
+
+type DishRatingTagOption struct {
+	DishRatingTagOption int64  `gorm:"primary_key;AUTO_INCREMENT;column:dishRatingTagOption;type:int;not null;" json:"dishRatingTagOption"`
+	DE                  string `gorm:"column:DE;type:text;default:de;not null;" json:"DE"`
+	EN                  string `gorm:"column:EN;type:text;default:en;not null;" json:"EN"`
+}
+
+type DishNameTagOption struct {
+	DishNameTagOption int64  `gorm:"primary_key;AUTO_INCREMENT;column:dishNameTagOption;type:int;not null;" json:"dishNameTagOption"`
+	DE                string `gorm:"column:DE;type:text;not null;" json:"DE"`
+	EN                string `gorm:"column:EN;type:text;not null;" json:"EN"`
+}
+
+type DishNameTagOptionExcluded struct {
+	DishNameTagOptionExcluded int64  `gorm:"primary_key;AUTO_INCREMENT;column:dishNameTagOptionExcluded;type:int;not null;" json:"dishNameTagOptionExcluded"`
+	NameTagID                 int64  `gorm:"foreignKey:dishNameTagOption;column:nameTagID;type:int;not null;" json:"nameTagID"`
+	Expression                string `gorm:"column:expression;type:text;" json:"expression"`
+}
+
+type DishNameTagOptionIncluded struct {
+	DishNameTagOptionIncluded int64  `gorm:"primary_key;AUTO_INCREMENT;column:dishNameTagOptionIncluded;type:int;not null;" json:"dishNameTagOptionIncluded"`
+	NameTagID                 int64  `gorm:"foreignKey:dishNameTagOption;column:nameTagID;type:int;not null;" json:"nameTagID"`
+	Expression                string `gorm:"column:expression;type:text;" json:"expression"`
+}
+
 //go:embed static_data
 var staticData embed.FS
 
@@ -41,7 +72,7 @@ func (m TumDBMigrator) migrate20231003000000() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "20231003000000",
 		Migrate: func(tx *gorm.DB) error {
-			if err := tx.Delete(&model.DishNameTagOption{}, &model.CafeteriaRatingTagOption{}, &model.DishNameTagOptionIncluded{}, &model.DishNameTagOptionExcluded{}).Error; err != nil {
+			if err := tx.Delete(&DishNameTagOption{}, &CafeteriaRatingTagOption{}, &DishNameTagOptionIncluded{}, &DishNameTagOptionExcluded{}).Error; err != nil {
 				return err
 			}
 			// todo re tables actually created?
@@ -62,7 +93,7 @@ func (m TumDBMigrator) migrate20231003000000() *gormigrate.Migration {
 			return err
 		},
 		Rollback: func(tx *gorm.DB) error {
-			err := tx.Delete(&model.DishNameTagOption{}, &model.CafeteriaRatingTagOption{}, &model.DishNameTagOptionIncluded{}, &model.DishNameTagOptionExcluded{}).Error
+			err := tx.Delete(&DishNameTagOption{}, &CafeteriaRatingTagOption{}, &DishNameTagOptionIncluded{}, &DishNameTagOptionExcluded{}).Error
 			return err
 		},
 	}
@@ -87,12 +118,12 @@ func setNameTagOptions(db *gorm.DB) {
 	for _, v := range tagsNames.MultiLanguageNameTags {
 		var parentId int64
 
-		parent := model.DishNameTagOption{
+		parent := DishNameTagOption{
 			DE: v.TagNameGerman,
 			EN: v.TagNameEnglish,
 		}
 
-		if err := db.Model(&model.DishNameTagOption{}).Session(&gorm.Session{Logger: logger.Default.LogMode(logger.Silent)}).Create(&parent).Error; err != nil {
+		if err := db.Model(&DishNameTagOption{}).Session(&gorm.Session{Logger: logger.Default.LogMode(logger.Silent)}).Create(&parent).Error; err != nil {
 			fields := log.Fields{"en": v.TagNameEnglish, "de": v.TagNameGerman}
 			log.WithError(err).WithFields(fields).Error("Error while creating tag")
 		}
@@ -106,9 +137,9 @@ func setNameTagOptions(db *gorm.DB) {
 func addNotIncluded(parentId int64, db *gorm.DB, v nameTag) {
 	for _, expression := range v.NotIncluded {
 		fields := log.Fields{"expression": expression, "parentId": parentId}
-		err := db.Model(&model.DishNameTagOptionExcluded{}).
+		err := db.Model(&DishNameTagOptionExcluded{}).
 			Session(&gorm.Session{Logger: logger.Default.LogMode(logger.Silent)}).
-			Create(&model.DishNameTagOptionExcluded{
+			Create(&DishNameTagOptionExcluded{
 				Expression: expression,
 				NameTagID:  parentId}).Error
 		if err != nil {
@@ -121,9 +152,9 @@ func addCanBeIncluded(parentId int64, db *gorm.DB, v nameTag) {
 	for _, expression := range v.CanBeIncluded {
 		fields := log.Fields{"expression": expression, "parentId": parentId}
 
-		err := db.Model(&model.DishNameTagOptionIncluded{}).
+		err := db.Model(&DishNameTagOptionIncluded{}).
 			Session(&gorm.Session{Logger: logger.Default.LogMode(logger.Silent)}).
-			Create(&model.DishNameTagOptionIncluded{
+			Create(&DishNameTagOptionIncluded{
 				Expression: expression,
 				NameTagID:  parentId,
 			}).Error
@@ -143,22 +174,22 @@ func setTagTable(path string, db *gorm.DB, tagType backend.ModelType) {
 
 	var insertModel *gorm.DB
 	if tagType == backend.DISH {
-		insertModel = db.Model(&model.DishRatingTagOption{})
+		insertModel = db.Model(&DishRatingTagOption{})
 	} else {
-		insertModel = db.Model(&model.CafeteriaRatingTagOption{})
+		insertModel = db.Model(&CafeteriaRatingTagOption{})
 	}
 
 	for _, v := range tagsDish.MultiLanguageTags {
 		fields := log.Fields{"de": v.TagNameGerman, "en": v.TagNameEnglish}
 		var createError error
 		if tagType == backend.CAFETERIA {
-			element := model.CafeteriaRatingTagOption{
+			element := CafeteriaRatingTagOption{
 				DE: v.TagNameGerman,
 				EN: v.TagNameEnglish,
 			}
 			createError = insertModel.Create(&element).Error
 		} else {
-			element := model.DishRatingTagOption{
+			element := DishRatingTagOption{
 				DE: v.TagNameGerman,
 				EN: v.TagNameEnglish,
 			}
