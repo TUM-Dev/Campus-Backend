ea68230645d02c1f6f8fe10e9d34959a3a213fcb
Tobias Jungmann <32565407+tobiasjungmann@users.noreply.github.com>
2023-10-06 20:37:14 +0200

    Canteen Batch Logging Silencing (#254)
    * move head count to canteen service
    
    * silence logging for batch db operations
    
    * renamed cafeteria service to cafeteria
    
    * Update server/backend/cafeteria_rating_db_initializer.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/cafeteria_rating_db_initializer.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * code review feedback
    
    ---------
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- server/backend/cafeteria.go
+++ server/backend/cafeteria.go
@@ -729,3 +729,20 @@ func (s *CampusServer) ListDishes(ctx context.Context, request *pb.ListDishesReq
 		Dish: results,
 	}, requestStatus
 }
+
+// GetCanteenHeadCount RPC Endpoint
+func (s *CampusServer) GetCanteenHeadCount(ctx context.Context, input *pb.GetCanteenHeadCountRequest) (*pb.GetCanteenHeadCountReply, error) {
+	data := model.CanteenHeadCount{Count: 0, MaxCount: 0, Percent: -1} // Initialize with an empty (not found) value
+	err := s.db.WithContext(ctx).Where(model.CanteenHeadCount{CanteenId: input.CanteenId}).FirstOrInit(&data).Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		log.WithError(err).Error("while querying the canteen head count for: ", input.CanteenId)
+		return nil, status.Error(codes.Internal, "failed to query head count")
+	}
+
+	return &pb.GetCanteenHeadCountReply{
+		Count:     data.Count,
+		MaxCount:  data.MaxCount,
+		Percent:   data.Percent,
+		Timestamp: timestamppb.New(data.Timestamp),
+	}, nil
+}
