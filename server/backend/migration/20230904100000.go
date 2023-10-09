c9801de25bb829bdccf14dd98e8f1dfa8db41fe7
Tobias Jungmann <32565407+tobiasjungmann@users.noreply.github.com>
2023-10-09 12:35:47 +0200

    Move Canteen Static Data in Migration (#251)
    * moved old functions in new migration
    
    * remove unnecessary checks whether tag exists
    
    * removed old initializer
    
    * delete previous data + cron entries fix
    
    * silenced logging of adding entries
    
    * registered the migration andfixed a few typos
    
    * fixed linting issues
    
    * added code review feedback
    
    * added state of models in migration
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/backend/migration/20231003000000.go
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * create missing tables in migration
    
    * fixed batch delete in migration
    
    * set fix version of migrated structs
    
    * formatting change
    
    * formatting change
    
    * removed unnecessary create models
    
    * removed further instances of hasTable
    
    ---------
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- server/backend/migration/20230904100000.go
+++ server/backend/migration/20230904100000.go
@@ -7,8 +7,18 @@ import (
 	"gorm.io/gorm"
 )
 
+// NewsSource struct is a row record of the newsSource table in the tca database
+type NewsSource struct {
+	Source int64       `gorm:"primary_key;AUTO_INCREMENT;column:source;type:int;"`
+	Title  string      `gorm:"column:title;type:text;size:16777215;"`
+	URL    null.String `gorm:"column:url;type:text;size:16777215;"`
+	FileID int64       `gorm:"column:icon;not null;type:int;"`
+	File   model.File  `gorm:"foreignKey:FileID;references:file;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Hook   null.String `gorm:"column:hook;type:char;size:12;"`
+}
+
 // migrate20230904100000
-// migrates the crontap from kino to movie crontab
+// migrates the crontab from kino to movie crontab
 func (m TumDBMigrator) migrate20230904100000() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "20230904100000",
@@ -23,8 +33,11 @@ func (m TumDBMigrator) migrate20230904100000() *gormigrate.Migration {
 			if err := SafeEnumMigrate(tx, &model.Crontab{}, "type", "movie"); err != nil {
 				return err
 			}
+			if err := tx.AutoMigrate(&NewsSource{}); err != nil {
+				return err
+			}
 			// tu film news source is now inlined
-			if err := tx.Delete(&model.NewsSource{Source: 2}).Error; err != nil {
+			if err := tx.Delete(&NewsSource{Source: 2}).Error; err != nil {
 				return err
 			}
 			return tx.Create(&model.Crontab{
@@ -44,7 +57,7 @@ func (m TumDBMigrator) migrate20230904100000() *gormigrate.Migration {
 			if err := SafeEnumMigrate(tx, &model.Crontab{}, "type", "kino"); err != nil {
 				return err
 			}
-			if err := tx.Create(&model.NewsSource{
+			if err := tx.Create(&NewsSource{
 				Source: 2,
 				Title:  "TU Film",
 				URL:    null.StringFrom("http://www.tu-film.de/programm/index/upcoming.rss"),
--- server/model/news_source.go
+++ server/model/news_source.go
@@ -20,7 +20,7 @@ type NewsSource struct {
 	Source int64       `gorm:"primary_key;AUTO_INCREMENT;column:source;type:int;"`
 	Title  string      `gorm:"column:title;type:text;size:16777215;"`
 	URL    null.String `gorm:"column:url;type:text;size:16777215;"`
-	FileID int64       `gorm:"column:icon;not null"`
+	FileID int64       `gorm:"column:icon;not null;type:int;"`
 	File   File        `gorm:"foreignKey:FileID;references:file;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Hook   null.String `gorm:"column:hook;type:char;size:12;"`
 }
