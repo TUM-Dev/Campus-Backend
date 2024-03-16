d737304381192a180d25fad393dee042cb10dd1e
Frank Elsinga <frank@elsinga.de>
2024-03-16 07:23:38 +0100

    tested better testing

--- server/backend/migration/20240317000000.go
+++ server/backend/migration/20240317000000.go
@@ -13,19 +13,22 @@ func tablesWithWrongCOLLATE() []string {
 }
 
 // migrate20240317000000
-// unified a variety of factors to not be different for no reason
+// unified all of our tables, the database and the fields to use `utf8mb4_unicode_ci` instead of the legacy `utf8mb3_general_ci` or `latin1`
 func migrate20240317000000() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "20240317000000",
 		Migrate: func(tx *gorm.DB) error {
+			// first migrate the db
 			if err := tx.Exec(fmt.Sprintf("ALTER DATABASE `%s` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci", os.Getenv("DB_NAME"))).Error; err != nil {
 				return err
 			}
+			// then set the tables
 			for _, t := range tablesWithWrongCOLLATE() {
 				if err := tx.Exec(fmt.Sprintf("ALTER TABLE `%s` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci", t)).Error; err != nil {
 					return err
 				}
 			}
+			// todo: then convert single columns in each table
 			return nil
 		},
 		Rollback: func(tx *gorm.DB) error {
