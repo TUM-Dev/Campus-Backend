c413255d98b4729423cd7b6355d5a1b57a59fbae
Frank Elsinga <frank@elsinga.de>
2024-03-16 07:17:42 +0100

    fixed more differences

--- server/backend/migration/20240317000000.go
+++ server/backend/migration/20240317000000.go
@@ -2,6 +2,7 @@ package migration
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
@@ -17,6 +18,9 @@ func migrate20240317000000() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "20240317000000",
 		Migrate: func(tx *gorm.DB) error {
+			if err := tx.Exec(fmt.Sprintf("ALTER DATABASE `%s` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci", os.Getenv("DB_NAME"))).Error; err != nil {
+				return err
+			}
 			for _, t := range tablesWithWrongCOLLATE() {
 				if err := tx.Exec(fmt.Sprintf("ALTER TABLE `%s` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci", t)).Error; err != nil {
 					return err
