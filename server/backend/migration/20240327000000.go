3e9f4d48f0290e4ff2605f045b1d610745327857
Frank Elsinga <frank@elsinga.de>
2024-03-27 22:21:22 +0100

    fixed `roomfinder_schedules`-pk apparently needing more fields to be unique

--- server/backend/migration/20240327000000.go
+++ server/backend/migration/20240327000000.go
@@ -46,7 +46,7 @@ func migrate20240327000000() *gormigrate.Migration {
 				return err
 			}
 			// roomfinder_schedules does not have a PK set
-			if err := tx.Exec("alter table roomfinder_schedules add constraint roomfinder_schedules_pk primary key (room_id)").Error; err != nil {
+			if err := tx.Exec("alter table roomfinder_schedules add constraint roomfinder_schedules_pk primary key (room_id, start, end)").Error; err != nil {
 				return err
 			}
 			return nil
