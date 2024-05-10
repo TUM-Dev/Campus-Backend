187d36bcd63d29aaed36196beae1e589d1d7dcce
Frank Elsinga <frank@elsinga.de>
2024-05-10 12:50:59 +0200

    added a migration to remove the chat (#355)
    * added a migration to remove the chat
    
    * fixed formatting issues
    
    * fixed a formatting mistake

--- server/backend/migration/migration.go
+++ server/backend/migration/migration.go
@@ -80,6 +80,7 @@ func manualMigrate(db *gorm.DB) error {
 		migrate20240327000000(),
 		migrate20240402000000(),
 		migrate20240405000000(),
+		migrate20240510000000(),
 	}
 	return gormigrate.New(db, gormigrateOptions, migrations).Migrate()
 }
