fdf57a9f3e12eb2f53a229c38d918d77421928c4
Frank Elsinga <frank@elsinga.de>
2024-08-23 01:34:26 +0200

    fixup

--- server/model/movie.go
+++ server/model/movie.go
@@ -1,23 +1,26 @@
 package model
 
 import (
+	"github.com/guregu/null"
 	"time"
 )
 
 // Movie stores all movies
 type Movie struct {
-	Movie       int64     `gorm:"primary_key;AUTO_INCREMENT;column:kino;type:int;not null;"`
-	Date        time.Time `gorm:"column:date;type:datetime;not null;"`
-	Created     time.Time `gorm:"column:created;type:timestamp;not null;default:CURRENT_TIMESTAMP"`
-	Title       string    `gorm:"column:title;type:text;not null;"`
-	Year        string    `gorm:"column:year;type:varchar(4);not null;"`
-	Runtime     string    `gorm:"column:runtime;type:varchar(40);not null;"`
-	Genre       string    `gorm:"column:genre;type:varchar(100);not null;"`
-	Director    string    `gorm:"column:director;type:text;not null;"`
-	Actors      string    `gorm:"column:actors;type:text;not null;"`
-	ImdbRating  string    `gorm:"column:rating;type:varchar(4);not null;"`
-	Description string    `gorm:"column:description;type:text;not null;"`
-	FileID      int64     `gorm:"column:cover;not null"`
-	File        File      `gorm:"foreignKey:FileID;references:file;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Link        string    `gorm:"column:link;type:varchar(190);not null;unique;"`
+	Id          int64       `gorm:"primary_key;AUTO_INCREMENT;column:kino;type:int;not null;"`
+	Date        time.Time   `gorm:"column:date;type:datetime;not null;"`
+	Created     time.Time   `gorm:"column:created;type:timestamp;not null;default:CURRENT_TIMESTAMP"`
+	Title       string      `gorm:"column:title;type:text;not null;"`
+	Year        null.String `gorm:"column:year;type:varchar(4)"`
+	Runtime     null.String `gorm:"column:runtime;type:varchar(40)"`
+	Genre       null.String `gorm:"column:genre;type:varchar(100)"`
+	Director    null.String `gorm:"column:director;type:text"`
+	Actors      null.String `gorm:"column:actors;type:text"`
+	ImdbRating  null.String `gorm:"column:rating;type:varchar(4)"`
+	Description string      `gorm:"column:description;type:text;not null;"`
+	Trailer     null.String `gorm:"column:trailer"`
+	FileID      int64       `gorm:"column:cover"`
+	File        File        `gorm:"foreignKey:FileID;references:file"`
+	Link        string      `gorm:"column:link;type:varchar(190);not null;unique;"`
+	Location    null.String `gorm:"column:location;default:null"`
 }
