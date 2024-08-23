76a08e6557aab80a9d4de3e4fe88f9a71cb1589e
Frank Elsinga <frank@elsinga.de>
2024-08-23 10:58:32 +0200

    fixed more nullability collumns

--- server/model/device.go
+++ server/model/device.go
@@ -10,16 +10,16 @@ import (
 type Device struct {
 	Device          int64       `gorm:"primary_key;AUTO_INCREMENT;column:device;type:int;" json:"device"`
 	Member          null.Int    `gorm:"column:member;type:int;" json:"member"`
-	UUID            string      `gorm:"column:uuid;type:varchar(50);" json:"uuid"`
+	UUID            string      `gorm:"column:uuid;type:varchar(50);not null" json:"uuid"`
 	Created         null.Time   `gorm:"column:created;type:timestamp;" json:"created"`
-	LastAccess      time.Time   `gorm:"column:lastAccess;type:timestamp;default:current_timestamp();" json:"last_access"`
-	LastAPI         string      `gorm:"column:lastApi;type:text;size:16777215;" json:"last_api"`
-	Developer       string      `gorm:"column:developer;type:enum('true','false');default:'false';" json:"developer"`
-	OsVersion       string      `gorm:"column:osVersion;type:text;size:16777215;" json:"os_version"`
-	AppVersion      string      `gorm:"column:appVersion;type:text;size:16777215;" json:"app_version"`
-	Counter         int32       `gorm:"column:counter;type:int;default:0;" json:"counter"`
+	LastAccess      time.Time   `gorm:"column:lastAccess;type:timestamp;default:CURRENT_TIMESTAMP;OnUpdate:CURRENT_TIMESTAMP;not null" json:"last_access"`
+	LastAPI         string      `gorm:"column:lastApi;type:text;size:16777215;not null" json:"last_api"`
+	Developer       string      `gorm:"column:developer;type:enum('true','false');default:'false';not null" json:"developer"`
+	OsVersion       string      `gorm:"column:osVersion;type:text;size:16777215;not null" json:"os_version"`
+	AppVersion      string      `gorm:"column:appVersion;type:text;size:16777215;not null" json:"app_version"`
+	Counter         int32       `gorm:"column:counter;type:int;default:0;not null" json:"counter"`
 	Pk              null.String `gorm:"column:pk;type:text;size:4294967295;" json:"pk"`
-	PkActive        string      `gorm:"column:pkActive;type:enum('true', 'false');default:'false';" json:"pk_active"`
+	PkActive        string      `gorm:"column:pkActive;type:enum('true', 'false');default:'false';not null" json:"pk_active"`
 	GcmToken        null.String `gorm:"column:gcmToken;type:text;size:65535;" json:"gcm_token"`
 	GcmStatus       null.String `gorm:"column:gcmStatus;type:varchar(200);" json:"gcm_status"`
 	ConfirmationKey null.String `gorm:"column:confirmationKey;type:varchar(35);" json:"confirmation_key"`
