7b77345fea0b42124d9fb83882336936b70c61f3
Frank Elsinga <frank@elsinga.de>
2024-08-23 16:32:18 +0200

    tested different rating tag configuration

--- server/model/canteen_rating_tag.go
+++ server/model/canteen_rating_tag.go
@@ -2,10 +2,10 @@ package model
 
 // CanteenRatingTag connects Canteens with ratings
 type CanteenRatingTag struct {
-	CanteenRatingTag      int64 `gorm:"primary_key;AUTO_INCREMENT;column:CafeteriaRatingTag"`
-	CorrespondingRatingID int64
-	CorrespondingRating   CanteenRating
-	TagID                 int64
-	Tag                   CanteenRatingTagOption
-	Points                int32
+	CanteenRatingTag int64 `gorm:"primary_key;AUTO_INCREMENT;column:CafeteriaRatingTag"`
+	RatingID         int64 `gorm:"column:corresponding_rating_id"`
+	Rating           CanteenRating
+	TagID            int64 `gorm:"column:tag_id"`
+	Tag              CanteenRatingTagOption
+	Points           int32 `gorm:"not null"`
 }
--- server/model/dish_rating_tag.go
+++ server/model/dish_rating_tag.go
@@ -1,8 +1,10 @@
 package model
 
 type DishRatingTag struct {
-	DishRatingTag       int64 `gorm:"primary_key;AUTO_INCREMENT;column:dishRatingTag;type:int;not null;" json:"dishRatingTag"`
-	CorrespondingRating int64 `gorm:"foreignKey:cafeteriaRating;column:parentRating;type:int;not null;" json:"parentRating"`
-	Points              int32 `gorm:"column:points;type:int;not null;" json:"points"`
-	TagID               int64 `gorm:"foreignKey:dishRatingTagOption;column:tagID;type:int;not null;" json:"tagID"`
+	DishRatingTag int64 `gorm:"primary_key;AUTO_INCREMENT;column:dishRatingTag"`
+	RatingID      int64 `gorm:"column:parentRating"`
+	Rating        CanteenRating
+	TagID         int64
+	Tag           CanteenRatingTagOption
+	Points        int32 `gorm:"not null"`
 }
