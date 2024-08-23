a8f6689523f8def3d6fbba6bbbabc34e3d3bcf11
Frank Elsinga <frank@elsinga.de>
2024-08-23 18:20:53 +0200

    finally figured out how rating tags should work

--- server/model/canteen_rating_tag.go
+++ server/model/canteen_rating_tag.go
@@ -3,9 +3,9 @@ package model
 // CanteenRatingTag connects Canteens with ratings
 type CanteenRatingTag struct {
 	CanteenRatingTag int64 `gorm:"primary_key;AUTO_INCREMENT;column:CafeteriaRatingTag"`
-	RatingID         int64 `gorm:"column:correspondingRating"`
+	RatingID         int64 `gorm:"column:correspondingRating;not null"`
 	//Rating           CanteenRating
-	TagID int64 `gorm:"column:tagID"`
+	TagID int64 `gorm:"column:tagID;not null"`
 	//Tag              CanteenRatingTagOption
 	Points int32 `gorm:"not null"`
 }
