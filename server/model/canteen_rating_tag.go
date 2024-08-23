7d7660970ca7b3498b0310d3907ffe79ec077625
Frank Elsinga <frank@elsinga.de>
2024-08-23 17:37:11 +0200

    tested different rating tags

--- server/model/canteen_rating_tag.go
+++ server/model/canteen_rating_tag.go
@@ -4,8 +4,8 @@ package model
 type CanteenRatingTag struct {
 	CanteenRatingTag int64 `gorm:"primary_key;AUTO_INCREMENT;column:CafeteriaRatingTag"`
 	RatingID         int64 `gorm:"column:corresponding_rating_id"`
-	Rating           CanteenRating
-	TagID            int64 `gorm:"column:tag_id"`
-	Tag              CanteenRatingTagOption
-	Points           int32 `gorm:"not null"`
+	//Rating           CanteenRating
+	TagID int64 `gorm:"column:tag_id"`
+	//Tag              CanteenRatingTagOption
+	Points int32 `gorm:"not null"`
 }
--- server/model/dish_rating_tag.go
+++ server/model/dish_rating_tag.go
@@ -3,8 +3,8 @@ package model
 type DishRatingTag struct {
 	DishRatingTag int64 `gorm:"primary_key;AUTO_INCREMENT;column:dishRatingTag"`
 	RatingID      int64 `gorm:"column:parentRating;not null"`
-	Rating        CanteenRating
-	TagID         int64 `gorm:"column:tagID;not null"`
-	Tag           CanteenRatingTagOption
-	Points        int32 `gorm:"not null"`
+	//Rating        CanteenRating
+	TagID int64 `gorm:"column:tagID;not null"`
+	//Tag           CanteenRatingTagOption
+	Points int32 `gorm:"not null"`
 }
