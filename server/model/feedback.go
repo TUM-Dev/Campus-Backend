8d7a2485540ad5e3348611d4616a599007136d61
Frank Elsinga <frank@elsinga.de>
2024-08-23 18:43:22 +0200

    tested simplifications to the Feedback model

--- server/model/feedback.go
+++ server/model/feedback.go
@@ -5,18 +5,18 @@ import (
 )
 
 type Feedback struct {
-	Id           int64       `gorm:"column:id;primary_key;AUTO_INCREMENT;type:int;not null;"`
-	ImageCount   int32       `gorm:"column:image_count;type:int;not null;"`
-	EmailId      string      `gorm:"column:email_id;type:text;not null"`
-	Recipient    string      `gorm:"column:receiver;type:text;not null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:receiver(255)"`
-	ReplyToEmail null.String `gorm:"column:reply_to_email;type:text;null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:reply_to_email(100)"`
-	ReplyToName  null.String `gorm:"column:reply_to_name;type:text;null"`
-	Feedback     string      `gorm:"column:feedback;type:text;not null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:feedback(255)"`
+	Id           int64       `gorm:"column:id;primary_key;AUTO_INCREMENT;not null;"`
+	ImageCount   int32       `gorm:"column:image_count;not null;"`
+	EmailId      string      `gorm:"column:email_id;not null"`
+	Recipient    string      `gorm:"column:receiver;not null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:receiver(255)"`
+	ReplyToEmail null.String `gorm:"column:reply_to_email;null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:reply_to_email(100)"`
+	ReplyToName  null.String `gorm:"column:reply_to_name;null"`
+	Feedback     string      `gorm:"column:feedback;not null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:feedback(255)"`
 	Latitude     null.Float  `gorm:"column:latitude;type:double;null;"`
 	Longitude    null.Float  `gorm:"column:longitude;type:double;null;"`
-	OsVersion    null.String `gorm:"column:os_version;type:text;null;"`
-	AppVersion   null.String `gorm:"column:app_version;type:text;null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:app_version(100)"`
-	Processed    bool        `gorm:"column:processed;type:boolean;default:false;not null;"`
+	OsVersion    null.String `gorm:"column:os_version;null;"`
+	AppVersion   null.String `gorm:"column:app_version;null;uniqueIndex:receiver_reply_to_feedback_app_version_uindex,expression:app_version(100)"`
+	Processed    bool        `gorm:"column:processed;default:false;not null;"`
 	Timestamp    null.Time   `gorm:"column:timestamp;type:datetime;default:current_timestamp();null;"`
 }
 
