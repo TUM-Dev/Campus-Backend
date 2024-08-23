2a44388d260ec9771f405e7a9d3689a8491fed54
Frank Elsinga <frank@elsinga.de>
2024-08-23 17:34:02 +0200

    different test

--- server/model/dish_rating_tag.go
+++ server/model/dish_rating_tag.go
@@ -2,9 +2,9 @@ package model
 
 type DishRatingTag struct {
 	DishRatingTag int64 `gorm:"primary_key;AUTO_INCREMENT;column:dishRatingTag"`
-	RatingID      int64 `gorm:"column:parentRating"`
+	RatingID      int64 `gorm:"column:parentRating;not null"`
 	Rating        CanteenRating
-	TagID         int64
+	TagID         int64 `gorm:"column:tagID;not null"`
 	Tag           CanteenRatingTagOption
 	Points        int32 `gorm:"not null"`
 }
