8d737adff7216fdc26f04212fad0a749de8edcd4
Frank Elsinga <frank@elsinga.de>
2024-08-23 10:23:15 +0200

    made sure that `Timestamp` has the appropriate defaults

--- server/model/dish_rating.go
+++ server/model/dish_rating.go
@@ -10,7 +10,7 @@ type DishRating struct {
 	DishID     int64     `gorm:"column:dishID;type:int;not null;" json:"dishID"`
 	Dish       Dish      `gorm:"foreignKey:dishID;references:dish;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Comment    string    `gorm:"column:comment;type:text;" json:"comment"`
-	Timestamp  time.Time `gorm:"column:timestamp;type:timestamp;not null;" json:"timestamp"`
+	Timestamp  time.Time `gorm:"column:timestamp;type:timestamp;not null;default:CURRENT_TIMESTAMP;OnUpdate:CURRENT_TIMESTAMP;" json:"timestamp"`
 	Image      string    `gorm:"column:image;type:text;" json:"image"`
 }
 
