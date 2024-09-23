2232e8fc6a80b6adbc756737844b12b92618c170
Frank Elsinga <frank@elsinga.de>
2024-09-24 06:09:34 +0800

    fixed a remakingly stupid typo

--- server/model/student_club_collection.go
+++ server/model/student_club_collection.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // StudentClubCollection stores what collection a club belongs to
 type StudentClubCollection struct {
 	gorm.Model
-	Name        string `gorm:"primaryKey;type:varchar(100)"`
+	Name        string `gorm:"type:varchar(100)"`
 	Language    string `gorm:"type:enum('German','English');default:'German'"`
 	Description string
 }
